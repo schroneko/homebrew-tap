50c135ee951a1b69595917242ac772316a4beda9
schroneko <schroneko@users.noreply.github.com>
2026-02-27 16:53:05 +0900

    Update NiceVoice to v0.1.2

--- Casks/nicevoice.rb
+++ Casks/nicevoice.rb
@@ -1,8 +1,8 @@
 cask "nicevoice" do
-  version "0.1.0"
-  sha256 :no_check
+  version "0.1.2"
+  sha256 "18dbbe05f556ecc4d50d89fce593b29cef91a1dac88277dc407dec38b104c649"
 
-  url "file:///Users/username/Sync/nicevoice-app/NiceVoice-#{version}.zip"
+  url "https://github.com/schroneko/homebrew-tap/releases/download/v#{version}/NiceVoice-#{version}.zip"
   name "NiceVoice"
   desc "Voice input app for macOS"
   homepage "https://github.com/schroneko/nicevoice-app"
