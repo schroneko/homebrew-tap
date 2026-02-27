781d80a099cfe6a48d525c8c7867ce88305a74c2
schroneko <schroneko@users.noreply.github.com>
2026-02-27 11:11:31 +0900

    use local path for nicevoice cask development

--- Casks/nicevoice.rb
+++ Casks/nicevoice.rb
@@ -1,8 +1,8 @@
 cask "nicevoice" do
   version "0.1.0"
-  sha256 "063ec8236bfa0bebca13b1e1bd432a514e41766465be148b290d1e49a8922b35"
+  sha256 :no_check
 
-  url "https://github.com/schroneko/homebrew-tap/releases/download/v#{version}/NiceVoice-#{version}.zip"
+  url "file:///Users/username/Sync/nicevoice-app/NiceVoice-#{version}.zip"
   name "NiceVoice"
   desc "Voice input app for macOS"
   homepage "https://github.com/schroneko/nicevoice-app"
